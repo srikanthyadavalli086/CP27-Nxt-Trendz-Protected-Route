54cb7eafde9d1ec4c609a7a509d6eb4007eb0af3
srikanth <srikanthyadavalli086@gmail.com>
2023-03-23 15:29:47 +0000

    edit 1

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,16 @@
+import {Route, Redirect} from 'react-router-dom'
+
+import Cookies from 'js-cookie'
+
+// import Home from '../Home'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
